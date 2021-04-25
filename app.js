02c243ac8a303764fb6af670ee15eff2e9b5a320
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-25 11:56:54 -0700

    animation added to headline

--- app.js
+++ app.js
@@ -10,5 +10,8 @@ let timeline = new TimelineMax();
 
  timeline
 .fromTo(first,1, {height: "0%"},{height:'80%',ease: Power2.easeInOut})
-.fromTo(first,1.2,{width:'100%'},{width:'80%',ease: Power2.easeInOut});
+.fromTo(first,1.2,{width:'100%'},{width:'80%',ease: Power2.easeInOut})
+.fromTo(logo,1.2,{x:"250px"},{x:"10px"} ,"-=2")
+.fromTo(headline,1,{opacity:"0"},{opacity:"1"},"-=1")
+.fromTo(headline ,1.2 ,{y:"-100%"},{y:"0%"});
 

c30b5c426492817a744e1df5ad9e907d04fbfce8
Fayyaz Alam <fayazalam.98@gmail.com>
2021-04-25 11:57:47 -0700

    timing changed

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ let timeline = new TimelineMax();
 .fromTo(first,1, {height: "0%"},{height:'80%',ease: Power2.easeInOut})
 .fromTo(first,1.2,{width:'100%'},{width:'80%',ease: Power2.easeInOut})
 .fromTo(logo,1.2,{x:"250px"},{x:"10px"} ,"-=2")
-.fromTo(headline,1,{opacity:"0"},{opacity:"1"},"-=1")
+.fromTo(headline,1,{opacity:"0"},{opacity:"1"},"-=1.2")
 .fromTo(headline ,1.2 ,{y:"-100%"},{y:"0%"});
 
